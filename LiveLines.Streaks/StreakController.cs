c8199b8421960755cdf93d43ab41debcb8ba23ce
James Potter <jamesrpotter1@gmail.com>
2022-02-20 17:59:02 +0000

    Create streak controller

---
+++ LiveLines.Streaks/StreakController.cs
@@ -0,0 +1,24 @@
+﻿using Extensions;
+using LiveLines.Api.Streaks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiveLines.Streaks;
+
+[Authorize]
+[ApiController, Route("api")]
+public class StreakController : ControllerBase
+{
+    private readonly IStreakService _streakService;
+
+    public StreakController(IStreakService streakService)
+    {
+        _streakService = streakService;
+    }
+
+    [HttpGet, Route("streak")]
+    public async Task<int> GetStreak()
+    {
+        return await _streakService.GetStreak(User.GetLoggedInUser());
+    }
+}
