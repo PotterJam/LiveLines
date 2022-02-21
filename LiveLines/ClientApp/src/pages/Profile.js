c1da4aa2bcabba45763ab0622638c30c3a90e7a0
Oliver Broomhall <obroomhall@gmail.com>
2022-02-21 20:18:06 +0100

    Fix profile

--- LiveLines/ClientApp/src/pages/Profile.js
+++ LiveLines/ClientApp/src/pages/Profile.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { FiLogOut } from 'react-icons/fi';
 import { getData, postData } from "../Api";
 import { UserContext } from '../auth/UserContext';
-import {PRIVACY_LEVEL} from "../components/Enum";
+import { PRIVACY_LEVEL } from "../components/Enum";
 
 export function Profile() {
   
@@ -25,34 +25,41 @@ export function Profile() {
         getUserProfile()
     }
   }, [user.authenticated]);
+  
+  useEffect(() => {
+    if (user.authenticated) {
+        updateProfile();
+    }
+  }, [linePrivacy]);
 
   const updateProfile = async () => {
-    const updatedProfileResp = await postData("api/user/profile", { linePrivacy: linePrivacy });
-  
+    const updatedProfileResp = await postData("api/user/profile", { 
+      linePrivacy: linePrivacy 
+    });
     const updatedProfile = await updatedProfileResp.json();
     setUsername(updatedProfile.username);
     setLinePrivacy(updatedProfile.linePrivacy);
-  }
-
-  const setPrivacySelection = e => {
-    setLinePrivacy(e.target.value);
   }
   
   return (
     <div className="h-max mx-auto border border-slate-300 bg-white w-max mt-10">
       <div className='flex flex-col items-center px-20 py-10'>
         <div className='text-3xl pb-5'>{username}</div>
+        <div className="flex space-x-4 py-4">
           <div>
-            <label className="pr-2">
+            <label>
               Default Line Privacy:
-              <select value={linePrivacy} onChange={setPrivacySelection}>
-                <option value={PRIVACY_LEVEL.PRIVATE}>{PRIVACY_LEVEL.PRIVATE}</option>
-                <option value={PRIVACY_LEVEL.UNLISTED}>{PRIVACY_LEVEL.UNLISTED}</option>
-                <option value={PRIVACY_LEVEL.PUBLIC}>{PRIVACY_LEVEL.PUBLIC}</option>
+              <select 
+                  value={linePrivacy} 
+                  onChange={e => setLinePrivacy(e.target.value)}
+              >
+                <option>{PRIVACY_LEVEL.PRIVATE}</option>
+                <option>{PRIVACY_LEVEL.UNLISTED}</option>
+                <option>{PRIVACY_LEVEL.PUBLIC}</option>
               </select>
             </label>
           </div>
-          <button onClick={updateProfile}>Update</button>
+        </div>
         <div>
           <span>Logout</span>
           <a href={logout}><FiLogOut className="ml-2 mb-1 inline-block"/></a>
