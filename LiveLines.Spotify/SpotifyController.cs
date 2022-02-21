c3f7cd5da28873c80c2e2314ed3ffab3265f2819
James Potter <jamesrpotter1@gmail.com>
2022-02-21 13:20:43 +0000

    Beginning of spotify oauth

--- LiveLines.Spotify/SpotifyController.cs
+++ LiveLines.Spotify/SpotifyController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Security.Cryptography;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LiveLines.Spotify;
@@ -6,9 +8,41 @@ namespace LiveLines.Spotify;
 [ApiController, Route("api")]
 public class SpotifyController : ControllerBase
 {
-    [AllowAnonymous]
+
+    private string GenerateRandomString(int length)
+    {
+        var bitCount = length * 6;
+        var byteCount = (bitCount + 7) / 8; // rounded up
+        var bytes = new byte[byteCount];
+        RandomNumberGenerator.Create().GetBytes(bytes);
+        return Convert.ToBase64String(bytes);
+    }
+    
+    [Authorize, Route("spotify/login")]
+    public IActionResult Login()
+    {
+        var csrfToken = GenerateRandomString(16);
+        Response.Cookies.Append(StateKey, csrfToken);
+        
+        const string scope = "user-read-private user-read-email";
+
+        var query = new QueryBuilder
+        {
+            {"response_type", "code"},
+            {"client_id", ClientId},
+            {"scope", scope},
+            {"redirect_uri", RedirectUri},
+            {"state", csrfToken}
+        };
+
+        return Redirect("https://accounts.spotify.com/authorize" + query);
+    }
+    
+    [AllowAnonymous, Route("spotify/callback")]
     public Task Callback()
     {
+        var body = Request.Body;
+        var request = Request;
         return Task.CompletedTask;
     }
 }
--- LiveLines/ClientApp/src/pages/Profile.js
+++ LiveLines/ClientApp/src/pages/Profile.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { FiLogOut } from 'react-icons/fi';
 import { getData, postData } from "../Api";
 import { UserContext } from '../auth/UserContext';
+import { BsSpotify } from 'react-icons/bs';
 
 export function Profile() {
   
@@ -9,6 +10,7 @@ export function Profile() {
 
   const [userInfo, setUserInfo] = useState({});
 
+  const loginSpotify = 'api/spotify/login';
   const logout = 'api/logout';
   
   const getUserProfile = async () => {
@@ -27,6 +29,7 @@ export function Profile() {
     <div className="h-max mx-auto border border-slate-300 bg-white w-max mt-10">
       <div className='flex flex-col items-center px-20 py-10'>
         <div className='text-3xl pb-5'>{userInfo.username}</div>
+        <a className='pb-3' href={loginSpotify}>Login to Spotify<BsSpotify className="ml-2 mb-1 inline-block"/></a>
         <div>
           <span>Logout</span>
           <a href={logout}><FiLogOut className="ml-2 mb-1 inline-block"/></a>
